c06af545cd6ee088e9dbf5ca7d3998f0fc6bf1ce
Shao Kang Seetoh <shaokangseetoh@Shaos-MacBook-Air-2.local>
2024-11-05 10:35:27 +0800

    added operation-manager-module

--- HoRSManagementClient/src/java/horsmanagementclient/OperationManagerModule.java
+++ HoRSManagementClient/src/java/horsmanagementclient/OperationManagerModule.java
@@ -1,5 +1,8 @@
 package horsmanagementclient;
 
+import entity.EmployeeEntity;
+import util.exception.InvalidAccessRightException;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,5 +13,17 @@ package horsmanagementclient;
  * @author shaokangseetoh
  */
 public class OperationManagerModule {
+    
+    // Insert relevant sessionbeans
+    private EmployeeEntity currentEmployee;
+    
+    
+    public OperationManagerModule() {
+    }
+    
+    // insert constructor with the appropriate sessionbean
+    
+    public void menuOperationManager() throws InvalidAccessRightException {
+    }
     
 }
